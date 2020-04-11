755ccd985c4b54a1ee20eb79ce0659439a4564c1
Babloo Mishra <babloo.mishra@tothenew.com>
2020-04-11 11:20:31 +0530

    Updates

---
+++ precache-manifest.66b7a2cd2ad931406f246cf108fc8723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2463c15a66bd7214ba5235065f9611",
+    "url": "//mapapplication/index.html"
+  },
+  {
+    "revision": "91357decb7a9165cece8",
+    "url": "//mapapplication/static/css/main.950f654f.chunk.css"
+  },
+  {
+    "revision": "74eab41163f7d9d3da4f",
+    "url": "//mapapplication/static/js/2.4a3425e3.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "//mapapplication/static/js/2.4a3425e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91357decb7a9165cece8",
+    "url": "//mapapplication/static/js/main.805b2521.chunk.js"
+  },
+  {
+    "revision": "b82efa77aac25fbbe853",
+    "url": "//mapapplication/static/js/runtime-main.306f2fe7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//mapapplication/static/media/logo.5d5d9eef.svg"
+  }
+]);
